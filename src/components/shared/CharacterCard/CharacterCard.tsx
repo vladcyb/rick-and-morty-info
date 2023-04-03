13584ebe6a9afa1d077bbb3888859e6193eeca2b
Vlad Tsibizov <vladtsibizov@yandex.ru>
2023-04-04 01:20:52 +0300

    Fix CharacterCard

--- src/components/shared/CharacterCard/CharacterCard.tsx
+++ src/components/shared/CharacterCard/CharacterCard.tsx
@@ -5,12 +5,24 @@ interface ICharacterCardProps {
   data: Character
 }
 
-export const CharacterCard = ({ data: { image, gender, name } }: ICharacterCardProps) => (
+type Gender = Character['gender']
+
+const genderToRussian: Record<Gender, string> = {
+  Female: 'женский',
+  Male: 'мужской',
+  Genderless: 'бесполый',
+  unknown: 'неизвестно',
+}
+
+export const CharacterCard = ({ data: { id, image, gender, name } }: ICharacterCardProps) => (
   <Card>
     <Card.Img src={image} alt={name} />
     <Card.Body>
       <Card.Title>{name}</Card.Title>
-      <Card.Text>{gender}</Card.Text>
+      <Card.Text>
+        <div>Пол: {genderToRussian[gender]}</div>
+        <div>Id: {id}</div>
+      </Card.Text>
     </Card.Body>
   </Card>
 )
