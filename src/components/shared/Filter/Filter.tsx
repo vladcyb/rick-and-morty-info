beb756a40bf5fe83805e6b9c55c0d3c1e8439e76
Vlad Tsibizov <vladtsibizov@yandex.ru>
2023-04-04 04:27:17 +0300

    Fix filter

--- src/components/shared/Filter/Filter.tsx
+++ src/components/shared/Filter/Filter.tsx
@@ -1,9 +1,13 @@
+import clsx from 'clsx'
 import { useState } from 'react'
+import Collapse from 'react-bootstrap/Collapse'
 import Form from 'react-bootstrap/Form'
 import { useSearchParams } from 'react-router-dom'
 import { useDebouncedCallback } from 'use-debounce'
 
 import { Nullable } from '@app/shared/types'
+
+import './AsideFilter.scss'
 
 interface IFilterProps<T extends string> {
   label: string
@@ -15,6 +19,7 @@ interface IFilterProps<T extends string> {
 export const Filter = <T extends string>({ label, filterKey, items, getItemLabel }: IFilterProps<T>) => {
   const [searchParams, setSearchParams] = useSearchParams()
   const [value, setValue] = useState<Nullable<T>>(searchParams.get(filterKey) as Nullable<T>)
+  const [open, setOpen] = useState(true)
 
   const handleChangeParams = useDebouncedCallback((status: string, checked: boolean, name: string) => {
     const newSearchParams = new URLSearchParams(searchParams)
@@ -23,23 +28,35 @@ export const Filter = <T extends string>({ label, filterKey, items, getItemLabel
   }, 300)
 
   return (
-    <Form.Group>
-      <Form.Label>{label}</Form.Label>
-      {items.map((item) => (
-        <Form.Check
-          type="checkbox"
-          label={getItemLabel(item)}
-          name={`${filterKey}-${item}`}
-          id={`${filterKey}-${item}`}
-          key={item}
-          onChange={(e) => {
-            const { checked } = e.target
-            setValue(checked ? e.target.name.replace(`${filterKey}-`, '') as T : null)
-            handleChangeParams(item, checked, item)
-          }}
-          checked={value === item}
-        />
-      ))}
+    <Form.Group className={clsx('aside-filter', { 'aside-filter_open': open })}>
+      <button
+        className="aside-filter__collapse-btn border"
+        onClick={() => setOpen(open => !open)}
+        type="button"
+      >
+        <small>
+          {label}
+        </small>
+      </button>
+      <Collapse in={open}>
+        <div className="py-2">
+          {items.map((item) => (
+            <Form.Check
+              type="checkbox"
+              label={getItemLabel(item)}
+              name={`${filterKey}-${item}`}
+              id={`${filterKey}-${item}`}
+              key={item}
+              onChange={(e) => {
+                const { checked } = e.target
+                setValue(checked ? e.target.name.replace(`${filterKey}-`, '') as T : null)
+                handleChangeParams(item, checked, item)
+              }}
+              checked={value === item}
+            />
+          ))}
+        </div>
+      </Collapse>
     </Form.Group>
   )
 }
